e704e6bb3c505fb0679c2a8d7d69dcf1d62a4ca1
Noor Jamal Hajbi <noor.hajbi@gmail.com>
2021-06-28 14:41:27 +0300

    push lab01

--- basics/Main.java
+++ basics/Main.java
@@ -1,30 +1,59 @@
 package basics;
 public class Main {
-    public static void main(String[] args) {
-        String greeting = "Welcome to my Java program.";
-        System.out.println(greeting);
-
-        int x = 7;
-        float f = 42.3f;
-        double d = 42.3;
-
-        // execute the function called "loopTo" and pass 10 as a parameter
-        loopTo(10);
-
-        int four = 4;
-        int result = multiplyByTwo(four);
-        System.out.println(four + " doubled is " + result);
+    public Main() {
     }
 
-    // Functions that return nothing have a return type of "void"
-    public static void loopTo(int max) {
-        for (int i = 0; i < max; i++) {
-            System.out.println("Loop #" + i);
+    public static String pluralize(String word, int num) {
+        return num != 0 && num <= 1 ? word : word + "s";
+    }
+
+    public static void flipNHeads(int n) {
+        int flipsCount = 0;
+        int headCount = 0;
+
+        for(int i = 1; i <= n; ++i) {
+            Random r = new Random();
+            double flips = r.nextDouble();
+            if (flips < 0.5D) {
+                System.out.println("tail");
+            } else {
+                System.out.println("head");
+                ++headCount;
+            }
+
+            ++flipsCount;
+        }
+
+        System.out.println("It took " + flipsCount + " flips to flip " + headCount + " head in a row.");
+    }
+
+    public static void clock() {
+        int perSec = 0;
+
+        while(true) {
+            int second;
+            String time;
+            do {
+                LocalDateTime now = LocalDateTime.now();
+                second = now.getSecond();
+                time = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+            } while(perSec == second);
+
+            perSec = second;
+            System.out.println(time);
         }
     }
 
-    // This function has a return type of "int"
-    public static int multiplyByTwo(int n) {
-        return n * 2;
+    public static void main(String[] args) {
+        int dogCount = 1;
+        System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
+        int catCount = 2;
+        System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
+        int turtleCount = 0;
+        System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
+        flipNHeads(4);
+        flipNHeads(1);
+        flipNHeads(6);
+        clock();
     }
 }
