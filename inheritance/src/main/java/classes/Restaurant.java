9edfec55d6e5cd46b2be14041194c2fe728f66b0
Noor Jamal Hajbi <noor.hajbi@gmail.com>
2021-07-05 23:03:14 +0300

    without 5th step, and testing

--- inheritance/src/main/java/classes/Restaurant.java
+++ inheritance/src/main/java/classes/Restaurant.java
@@ -1,46 +1,65 @@
 package classes;
 
+import java.util.List;
+
 public class Restaurant {
-
-
+    //(1)
     //Test that your classes.Restaurant constructor is behaving reasonably.
     //Implement a reasonable toString method for Restaurants.
     //Write a test to create an instance of classes.Restaurant and ensure that its toString is working properly.
     private final String name;
-    private int stars=1; //-5
-    private double price;
+    private int stars = 1; //-5
+    private double priceCategory;
+    //    public Review review;
+    private List<Review> review;
 
-    public Restaurant(String name, double price) {
+
+//(5)
+// When you associate a review with a restaurant, 
+//that restaurant’s star rating should change. 
+//Ensure that your addReview method updates the Restaurant star rating appropriately,
+// and add tests.
+// (In particular, ensure that if you’re trying to call addReview when the restaurant and the review are already associated, the star rating of the restaurant does not update.)
+
+    public Restaurant(String name, double priceCategory) {
         this.name = name;
-        this.price = price;
+        this.priceCategory = priceCategory;
+    }
+
+    public List<Review> getReviews() {
+        return review;
     }
 
     public String getName() {
         return name;
     }
 
-    public double getPrice() {
-        return price;
+    public String getPrice() {
+        return priceCategory + "$";
     }
 
     public int getStar() {
         return stars;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
+    public void setPrice(double priceCategory) {
+        this.priceCategory = priceCategory;
     }
 
     public void setStar(int stars) {
         this.stars = stars;
     }
 
+
     @Override
     public String toString() {
         return "Restaurant{" +
                 "name='" + name + '\'' +
                 ", stars=" + stars +
-                ", price=" + price +"$"+
+                ", price=" + priceCategory + "$" +
+                "\n" + review +
                 '}';
     }
+
+
 }
--- inheritance/src/main/java/classes/Review.java
+++ inheritance/src/main/java/classes/Review.java
@@ -1,50 +1,55 @@
 package classes;
 
 import java.util.*;
-//Create a class to represent a Review. Each Review should have a body, an author, and a number of stars.
-//        Write a Review constructor.
+
 //        Test that your constructor is working reasonably.
 //        Implement a reasonable toString method for Reviews.
 //        Write a test to create an instance of Review and ensure that its toString is working properly.
 public class Review {
-    private List<Review> review;
+    private String body; //0-5
+    private final String author;
+    private int stars;
 
-//    private final String name;
-//    private int stars; //0-5
-//    private double price;
-//
-//    public Restaurant(String name, double price) {
-//        this.name = name;
-//        this.price = price;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public double getPrice() {
-//        return price;
-//    }
-//
-//    public int getStar() {
-//        return stars;
-//    }
-//
-//    public void setPrice(double price) {
-//        this.price = price;
-//    }
-//
-//    public void setStar(int stars) {
-//        this.stars = stars;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Restaurant{" +
-//                "name='" + name + '\'' +
-//                ", stars=" + stars +
-//                ", price=" + price +"$"+
-//                '}';
-//    }
+    public Review(String body, String author, int stars) {
+        this.body = body;
+        this.author = author;
+        this.stars = checkRate(stars);
+    }
 
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
+
+    private int checkRate(int stars) {
+        if (stars >= 1 && stars <= 5)
+            return stars;
+        else
+            return -1;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                ", body='" + body + '\'' +
+                ", author='" + author + '\'' +
+                ", stars='" + stars + '\'' +
+                '}';
+    }
 }
